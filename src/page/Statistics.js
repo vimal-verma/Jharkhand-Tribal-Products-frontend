a9d67796dc791b25a38fed27ed6112d74ee57ea5
vimal kumar <kumarvimal558@gmail.com>
2022-05-22 22:43:58 +0530

    some update

--- src/page/Statistics.js
+++ src/page/Statistics.js
@@ -6,6 +6,27 @@ import Lottie from '../utils/lottie'
 import developer from '../assets/groth.json'
 import axios from 'axios'
 
+import {
+    Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+} from 'chart.js';
+import { Pie, Line } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
 export default function Statistics() {
     var token = localStorage.getItem('token')
     // if (token) {
@@ -34,7 +55,7 @@ export default function Statistics() {
         item.product.forEach(product => {
             producname.push(product.url)
             producprice.push(product.price)
-            purchasetime.push(item.createdAt)
+            purchasetime.push(item.createdAt.substring(0, 10))
         })
 
     })
@@ -43,6 +64,86 @@ export default function Statistics() {
     console.log(producprice)
     console.log(purchasetime)
 
+    const counts = {};
+    const pptime = {};
+
+    for (const num of producname) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    for (const num of purchasetime) {
+        pptime[num] = pptime[num] ? pptime[num] + 1 : 1;
+    }
+
+    console.log(counts)
+
+    const unpname = []
+    const unpcount = []
+    const ppdate = []
+    const ppcount = []
+
+    for (let key in counts) {
+        if (counts.hasOwnProperty(key)) {
+            console.log(key, counts[key]);
+            unpname.push(key)
+            unpcount.push(counts[key])
+        }
+    };
+
+    for (let key in pptime) {
+        if (pptime.hasOwnProperty(key)) {
+            console.log(key, pptime[key]);
+            ppdate.push(key)
+            ppcount.push(pptime[key])
+        }
+    };
+    console.log(ppcount)
+    console.log(ppdate)
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        // // console.log(color)
+        return color;
+    };
+
+    var tagcol = unpname.map(() => { return getRandomColor() })
+
+
+    const data = {
+        labels: unpname,
+        datasets: [
+            {
+                label: '# of Votes',
+                data: unpcount,
+                backgroundColor: tagcol,
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const linedata = {
+        ppdate,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: ppcount,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Dataset 2',
+                data: ppcount,
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
+
 
     return (
         <div>
@@ -50,6 +151,10 @@ export default function Statistics() {
             <div className="dashome">
                 <div className="dashleft">
                     <h1>Product Statistics</h1>
+                    <div style={{ width: '500px' }}>
+                        <Pie data={data} />
+                    </div>
+                    <Line data={linedata} />
                 </div>
                 <div className="dashright">
                     <Lottie animationData={developer} />
